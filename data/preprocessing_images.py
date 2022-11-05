678dd102c2270252b63d43f0b6cc844ea1579dec
anesnabti <108154377+anesnabti@users.noreply.github.com>
2022-11-05 20:50:59 +0100

    Pre-process Images

---
+++ data/preprocessing_images.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+import matplotlib.image as mpimg
+import pathlib
+import os
+import sys
+import tensorflow as tf
+PARENT_PATH = str(pathlib.Path().parent.resolve())
+sys.path.append(PARENT_PATH + "\\cfg")
+from config import info, debug, warning, log_config
+
+"""
+This file aims to us to preprocess all images in our data. 
+The purpose is to crop images and resize them to (256 x 256)
+"""
+
+PATH = PARENT_PATH + "\\data"
+Nbr_images = 202599
+SIZE_IMG = 256
+
+log_config("preprocessing_images")
+
+def preprocessing_images ():
+
+    #verifying if all images are in data
+    if len(os.listdir(PATH + "\\img_align_celeba")) != Nbr_images:
+        debug("You do not have all images ! Please Check")
+        return
+    else: 
+        info("OK : All images are downloded")
+
+    print("############ Reading images ##############")
+    read_img = []
+    for i in range (1, Nbr_images + 1) :
+        if i % 10000 == 0:
+            print('iteration :',i)
+        read_img.append(mpimg.imread(PATH + "\\img_align_celeba\\%06i.jpg" % i)[20:-20])     # %06i% means that we have a number of 6 digits | we do [20 : -20] to crop images into 178 x 178
+
+    if len(read_img) != Nbr_images:
+        debug(f"Found {len(read_img)}, must have {Nbr_images} ")
+        raise Exception (f"Found {len(read_img)}, must have {Nbr_images} ")
+    else: 
+        info("All images have been read")
+
+    # Resizing images : 
+    print('############# Resizing Images #############')
+    resize_img = []
+    for i,img in enumerate(read_img): 
+        if i % 10000 == 0:
+            print('iteration :',i)
+
+        if img.shape != (178,178,3):
+            debug("Error cropped image")
+            raise Exception (" Image %06i%  .does not been cropped correctly % i")
+
+        img = cv2.resize(img, (SIZE_IMG, SIZE_IMG), interpolation=cv2.INTER_LANCZOS4)
+        assert img.shape == (SIZE_IMG, SIZE_IMG, 3)
+        resize_img.append(img)
+
+    images = np.array(resize_img)
+    info("All images are resized")
+    data_images = tf.convert_to_tensor(images, np.float32)
+    assert data_images.shape == (Nbr_images, SIZE_IMG, SIZE_IMG, 3)
+
+    # Saving this image
+    try : 
+        np.save(PATH + "\\IMAGES", data_images)
+        info("Model saved correctly")
+    except : 
+       debug("Images are not saved")
+    
+
+
+
+
+if __name__ == '__main__':
+    preprocessing_images()
+
+
+
