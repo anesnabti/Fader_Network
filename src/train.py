223686b41a39edbdab04187ef24a0f768ccd96af
anesnabti <108154377+anesnabti@users.noreply.github.com>
2022-11-27 23:35:17 +0100

    train model

--- src/train.py
+++ src/train.py
@@ -6,25 +6,23 @@ import sys
 import pathlib
 from fader_networks import GAN
 from model import encoder, decoder, discriminator
-sys.path.append(str(pathlib.Path().parent.resolve()) + "\\cfg")
-from config import debug, info, warning, log_config, PATH, epochs, batch_size
+sys.path.append( os.path.dirname(os.path.abspath(__file__))[:-3] + "\\cfg")
+from config import debug, info, warning, log_config, PATH
 import glob
 Nbr_images = 202599
 log_config('train')
 image_path = glob.glob(PATH + '\\data\\train' + '\\*.jpg')
-
 from fader_networks import GAN
-
 
 class Train:
 
-    def __init__ (self, num_attr, valeur, lr):
+    def __init__ (self, lr):
  
         self.image_path = glob.glob(PATH + '\\data\\train' + '\\*.jpg')
-        self.gan = GAN(num_attr, valeur)
+        # self.gan = GAN(num_attr, valeur)
         self.lr = lr
         self.attr = np.load(PATH + "\\data\\train\\train_images_att.npy")
-        self.gan = GAN(encoder=encoder, decoder=decoder, discriminator=discriminateur)
+        self.gan = GAN(encoder=encoder, decoder=decoder, discriminator=discriminator)
 
         
     
@@ -44,7 +42,7 @@ class Train:
     def training(self, epochs, batch_size):
 
         nbr_itr_per_epoch = int(len(np.array(self.image_path))/batch_size)
-         
+        info('start training') 
         for epoch in range (epochs):
          
             for i in range (nbr_itr_per_epoch):
@@ -52,12 +50,16 @@ class Train:
 
                 imgs = self.image_batch(batch_size, i)
                 atts = self.attr[i*batch_size : (i+1)*batch_size]
-                # for img, att in zip(np.array(imgs), atts):
-                self.gan.train_step(imgs, atts, lambda_e)
+                for img, att in zip(np.array(imgs), atts):
+                    loss_model = self.gan.train_step(img, att, lambda_e)
 
+            print(f'epoch : {epoch}  --------   loss = {loss_model}')
 
+        info(f"epoch: {epoch} finished OK")
 
 # if __name__ == '__main__':
-#     training = Train([6],[1],0.01)  
-#     training.training(3,10)  
+#     training = Train(0.01)  
+#     training.training(epochs=epochs, batch_size=batch_size)  
+
+
 
