a8ee57d1766e6d3a7be06a045ffe7e39cbe538ee
anesnabti <108154377+anesnabti@users.noreply.github.com>
2022-12-01 02:49:15 +0100

    MAJ

--- train_shell.bat
+++ train_shell.bat
@@ -1,3 +1,4 @@
 cd src
-python main_train.py --learning_rate 0.01 --epochs 3 --batch_size 32 --attributs Smiling Young Pointy_Nose
+python main_train.py --learning_rate 0.01 --epochs 3 --batch_size 32 --attributs Smiling Young Pointy_Nose --weights ""
+:: ../utils/models/discriminator_Smiling_Young_Pointy_Nose_30-11-2022_14h06.h5 ../utils/models/AutoEncoder_Smiling_Young_Pointy_Nose_30-11-2022_14h06.h5
 @pause
