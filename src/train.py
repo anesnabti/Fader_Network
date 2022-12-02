af77ff38e297c0cde4b1d7a81839c1cbcff0845e
anesnabti <108154377+anesnabti@users.noreply.github.com>
2022-12-02 02:07:07 +0100

    MAJ

--- src/train.py
+++ src/train.py
@@ -5,7 +5,6 @@ import os
 import sys
 import pathlib
 from datetime import datetime as date
-from load_data import Loader
 from fader_networks import GAN
 from model import Encoder, Decoder, Discriminator
 sys.path.append( os.path.dirname(os.path.abspath(__file__))[:-3] + "\\cfg")
@@ -16,68 +15,42 @@ Nbr_images = 202599
 log_config('train')
 image_path = glob.glob(PATH + '\\data\\train' + '\\*.jpg')
 from fader_networks import GAN
-import math
 
-def plot_images(x,y=None, indices='all', columns=12, x_size=1, y_size=1,
-                cm='binary',y_padding=0.35, spines_alpha=1,
-                fontsize=20, save_as='auto'):
-    """
-    Plot original images
-    Show some images in a grid, with legends
-    args:
-        x             : images - Shapes must be (-1,lx,ly) (-1,lx,ly,1) or (-1,lx,ly,3)
-        y             : real classes or labels or None (None)
-        indices       : indices of images to show or None for all (None)
-        columns       : number of columns (12)
-        x_size,y_size : figure size (1), (1)
-        cm            : Matplotlib color map (binary)
-        y_padding     : Padding / rows (0.35)
-        font_size     : Font size in px (20)
-        save_as       : Filename to use if save figs is enable ('auto')
-    """
-    if indices=='all': indices=range(len(x))
-    draw_labels = (y is not None)
-    rows        = math.ceil(len(indices)/columns)
-    fig=plt.figure(figsize=(columns*x_size, rows*(y_size+y_padding)))
-    n=1
-    for i in indices:
-        axs=fig.add_subplot(rows, columns, n)
-        n+=1
-        # ---- Shape is (lx,ly)
-        if len(x[i].shape)==2:
-            xx=x[i]
-        # ---- Shape is (lx,ly,n)
-        if len(x[i].shape)==3:
-            (lx,ly,lz)=x[i].shape
-            if lz==1: 
-                xx=x[i].reshape(lx,ly)
-            else:
-                xx=x[i]
-        img=axs.imshow(xx,   cmap = cm, interpolation='lanczos')
-        """
-        axs.spines['right'].set_visible(True)
-        axs.spines['left'].set_visible(True)
-        axs.spines['top'].set_visible(True)
-        axs.spines['bottom'].set_visible(True)
-        axs.spines['right'].set_alpha(spines_alpha)
-        axs.spines['left'].set_alpha(spines_alpha)
-        axs.spines['top'].set_alpha(spines_alpha)
-        axs.spines['bottom'].set_alpha(spines_alpha)
-        """
+
+class Loader():
+    def __init__ (self):
+# A revoir 
+        self.AVAILABLE_ATTR = [
+            "5_o_Clock_Shadow", "Arched_Eyebrows", "Attractive", "Bags_Under_Eyes", "Bald",
+            "Bangs", "Big_Lips", "Big_Nose", "Black_Hair", "Blond_Hair", "Blurry", "Brown_Hair",
+            "Bushy_Eyebrows", "Chubby", "Double_Chin", "Eyeglasses", "Goatee", "Gray_Hair",
+            "Heavy_Makeup", "High_Cheekbones", "Male", "Mouth_Slightly_Open", "Mustache",
+            "Narrow_Eyes", "No_Beard", "Oval_Face", "Pale_Skin", "Pointy_Nose",
+            "Receding_Hairline", "Rosy_Cheeks", "Sideburns", "Smiling", "Straight_Hair",
+            "Wavy_Hair", "Wearing_Earrings", "Wearing_Hat", "Wearing_Lipstick",
+            "Wearing_Necklace", "Wearing_Necktie", "Young"
+        ]
+
+    def normalize (self,image):  
+        return image/127.5 - 1 
+
+
+
+    def Load_Data(self, batch_size, itr, attributs, mod = 'train'):
+
+        image_path = glob.glob(PATH + '\\data\\' + mod + '\\*.jpg')
+        attributes_path = glob.glob(PATH + '\\data\\' + mod + '\\*.npy')
+        attributes = np.load(attributes_path[0])
+        ind = []
+        for i in attributs:
+            ind.append(self.AVAILABLE_ATTR.index(i)+1)
+        tmp_img = []
+        tmp_attr = []
         
-        for spine in ['bottom', 'left','top','right']:
-            axs.spines[spine].set_visible(False)
-        
-        axs.set_yticks([])
-        axs.set_xticks([])
-
-        if draw_labels:
-            axs.set_xlabel(y[n-2],fontsize=fontsize/2)
-
-    # a de-commenter pour enregistrer les images generer par notre model 
-    plt.savefig(PATH + "\\utils\\result_train\\im1.png")
-    # cv2.imwrite(PATH + "\\utils\\result_train\\im2.png", np.array(xx))
-    # plt.show()
+        for i in range (batch_size):
+            tmp_img.append(self.normalize(cv2.imread(image_path[i + itr * batch_size])))
+            tmp_attr.append(attributes[i + itr*batch_size][ind])
+        return np.array(tmp_img), np.array(tmp_attr).astype(float)
 
 
 
@@ -92,34 +65,46 @@ class Train:
         self.gan = GAN(encoder=Encoder(), decoder=Decoder(nbr_attr = self.nbr_attr,disc = True), discriminator=Discriminator(self.nbr_attr))
 
 
-    def result_train(self, real_image, reconstruct_image, epochs, nbr_itr_epoch, nbr_img=5):
-        real_image = np.array(real_image)* 127.5 + 1
-        reconstruct_image = np.array(reconstruct_image)*127.5 + 1
-        I = []
+    def result_train(self, real_image, reconstruct_image, epochs, nbr_itr_epoch, nbr_img=4):
+        real_image = (np.array(real_image)+1)*127.5
+        reconstruct_image = (np.array(reconstruct_image) + 1)*127.5 
+        I, img_list = [], []
         for i in range (nbr_img):
             I.append(real_image[i])
             for j in range (epochs):
                 I.append(reconstruct_image[j*(nbr_itr_epoch) + i])
 
+        # print(type(I[1]))
+        # exit()
+        for i in range (int(len(I)/(epochs+1))):
+            img_list.append(I[i*epochs + i : epochs*(i+1) + i + 1])
 
-        plot_images(np.array(I),columns=epochs+1)
+        img_list = np.array(img_list)
+        
+        
+        im_list = [img_list[0],
+                    img_list[1],
+                    img_list[2],
+                    img_list[3]]
+        I_concat = cv2.vconcat([cv2.hconcat(im) for im in im_list])
+        # plot_images(np.array(I),columns=epochs+1)
+        cv2.imwrite(PATH + f"\\utils\\result_train\\train_img_{date.today().strftime('%d-%m-%Y-%Hh%M')}.png", I_concat)
 
 
 
-
-
-
-    def training(self, epochs, batch_size, weights=""):
+    def training(self, epochs, batch_size):
         name_attributs = "_".join(self.attributs)
         ld = Loader()
-        nbr_itr_per_epoch = 5 #int(len(self.image_path)/batch_size)
+        nbr_itr_per_epoch = 500 #int(len(self.image_path)/batch_size)
         real_image = []
         reconstruct_image = []
-        info('Compiling Model')
-        if weights =="":
-            self.gan.compile()
-        else:
-            self.gan = self.gan.load_weights(weights)
+        self.gan.compile()
+
+        # info('Compiling Model')
+        # if weights ==" ":
+        #     self.gan.compile()
+        # else:
+        #     self.gan = self.gan.load_weights(weights)
 
 
         info('start training') 
@@ -136,6 +121,7 @@ class Train:
                 reconstruct_image.append(x_reconstruct[-1])
 
 
+
         self.result_train(real_image, reconstruct_image, epochs, nbr_itr_per_epoch)
        
         if not os.path.exists(PATH + f"\\utils\\models\\Model_{name_attributs}_{date.today().strftime('%d-%m-%Y')}"):
@@ -143,7 +129,7 @@ class Train:
         
         self.gan.save_weights(PATH + f"\\utils\\models\\Model_{name_attributs}_{date.today().strftime('%d-%m-%Y')}\\")
 
-        info(f"epoch: {epoch} finished OK")
+        info(f"epoch: {epoch + 1} finished OK")
 
 
     # def training(self, epochs, batch_size, attributs):
